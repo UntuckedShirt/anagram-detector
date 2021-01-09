02c57b0c560e1850925f3eaae2cc479f1b7095fa
Nathan Miller <natem31190@gmail.com>
2021-01-08 19:27:46 -0500

    commit tree f33e8a4e3a1308cc840f9db7e46c803890462ee6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+
+class Anagram
+
+    def initialize(word)
+            @word = word
+    end
+    def match(array)
+      
+        array.select do |item|
+            item.split("").sort == @word.split("").sort
+            
+        end
+        
+    end
+   
+end
